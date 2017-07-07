08d9c26840a2374cb1920dd1eb2a3e98e93e48a5
manuellf2h <manuellf2h@gmail.com>
2017-07-06 20:16:03 -0400

    Metodos listoco
    ahora si funcionando bien y limpio

--- Tarea/src/Metodos.java
+++ Tarea/src/Metodos.java
@@ -110,7 +110,7 @@ class Cinta{
 
 public class Metodos{
 
-	//Verifica si se encuentra un estado qi en la maquina:
+	//busca si se encuentra el estado qi
 	public static boolean Estaqi(ArrayList<Estado> mt,String qi){
 				for(Estado o: mt){
 					if(o.qi.equals(qi)) return true;	
@@ -170,6 +170,7 @@ public class Metodos{
                         Element e = (Element) unNodoHijo;
                        	String qi= e.getElementsByTagName("qi").item(0).getTextContent();
             			
+            			try{
                         	String si= e.getElementsByTagName("si").item(0).getTextContent();
                         	String sj= e.getElementsByTagName("sj").item(0).getTextContent();
                         	String qj= e.getElementsByTagName("qj").item(0).getTextContent();
@@ -181,29 +182,45 @@ public class Metodos{
 							else 
 								mt=InsertarEstado(mt,qi,si,sj,qj,movimiento);
 						//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+                    	}catch(Exception spe){
+                    		//algun problema del archivo xml mal formado.
+                    	}
+
                     }
                }
-            mt=ComprobarEstados(mt);// comprueba si se encuentra todos los estados en la maquina.
+            mt=ComprobarEstados(mt);
        		return mt;
  
     }
 
     //Se encarga que en la mt esten todos los estados(para los estados sin transicion);
     public static ArrayList<Estado> ComprobarEstados(ArrayList<Estado> mt){
-		for(Estado o: mt){
+    	ArrayList<String> para_insertar=new ArrayList<String>();
+    	boolean aux2=false;	
+		for(Estado o:mt){
             	try{
            			for (Transicion t: o.todasTransiciones) {
- 	          			if(!(Estaqi(mt,t.qj))) mt=InsertarEstadoSinTransicion(mt,t.qj);
+ 	          			if(!(Estaqi(mt,t.qj))){
+ 	          				String qi_aux= t.qj;
+ 	          				for(String a:para_insertar){
+										if(a.equals(qi_aux)) aux2=true;			
+							}
+							if(!aux2) para_insertar.add(qi_aux);
+							aux2=false;
+ 	          			}
            			}
             	}catch(Exception spe){
 	  				
            		}
         }
+		for(String o:para_insertar){
+				mt=InsertarEstadoSinTransicion(mt,o);		
+		}       
         return mt;    		
 
     }
 
-    //crea la cinta a partir de un string ingresado por el usario.
     public static Cinta CrearCinta(String cadena){
     	Cinta cadenas=new Cinta();
 		String[] separada = cadena.split(""); 
@@ -219,11 +236,12 @@ public class Metodos{
     public static boolean verificarCadena(ArrayList<Estado> mt,Cinta cadena, String qii,NodoCinta caracter){
     	for(Estado o:mt){
     			if(o.qi.equals(qii)){
-    				//En caso de que no tenga transiciones(estado sin transicion) y o.todasTransiciones este null por eso el try;
+    				//En caso de que no tenga transiciones y o.todasTransiciones este null por eso el try;
     					try{
     						for (Transicion t: o.todasTransiciones) {
  	  							if(t.si.equals(caracter.simbolo)){
  	          						caracter.simbolo=t.sj;
+ 	          						//System.out.println("movimiento= "+t.movimiento);
  	          						if(!t.movimiento.equals("E")){
  	          							NodoCinta aux=cadena.moverse(caracter,t.movimiento);
  	          							return verificarCadena(mt,cadena,t.qj,aux);
@@ -239,8 +257,7 @@ public class Metodos{
            				}
            			
       				if(o.estadoFinal==true) return true;
-           			else return false;
-           						
+           			else return false;				
 
     			}
     	}
@@ -265,5 +282,4 @@ public class Metodos{
 
     }
  
-
 }
